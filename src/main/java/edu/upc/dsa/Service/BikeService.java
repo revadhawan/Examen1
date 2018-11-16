6f116e2661de1eb45f8bb093008fa39b2a1918c4
revadhawan <43339571+revadhawan@users.noreply.github.com>
2018-11-16 15:36:20 +0100

    Update BikeService.java

--- src/main/java/edu/upc/dsa/Service/BikeService.java
+++ src/main/java/edu/upc/dsa/Service/BikeService.java
@@ -15,37 +15,89 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Api(value = "/tracks", description = "Endpoint to Track Service")
-@Path("/tracks")
+@Api(value = "/bikes", description = "Endpoint to Bike Service")
+@Path("/bikes")
 public class BikeService {
     final static Logger log = Logger.getLogger(BikeService.class.getName());
+    
     //Declarar interficie
     private MyBike tm;
-    //Importante
+    
+    
     public BikeService() {
         this.tm = MyBikeImpl.getInstance();
         if (tm.size()==0) {
             this.tm.addUser("Pepe", "Luis", "Garcia");
 
     }
-    //Dame todos los tracks
+        
+   @POST
+    @ApiOperation(value = "Add User", notes = "Users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful")
+    })
+        
+    @Path("/addUser")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addUser(User u) {
+        String idUser = u.getIdUser();
+        String name = u.getName();
+        String surname = u.getSurname();
+        this.mb.addUser(idUser, name, surname);
+
+        return Response.status(201).build();
+    }
+
+        
+         
+    @POST
+    @ApiOperation(value = "Add Station", notes = "Stations")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful")
+    })
+        
+    @Path("/addStation")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addStation(Station s) {
+        String idStation = s.getIdUser();
+        String description = s.getDescription();
+        int max = s.getMax();
+        double lat = s.getLat();
+        double lon = s.getLon();
+        this.mb.addStation(idStation, description, max, lat, lon);
+
+        return Response.status(201).build();
+    }    
+        
+    //Obtener bikes de una station sorted by kms
     @GET
-    @ApiOperation(value = "get all Users", notes = "asdasd")
+    @ApiOperation(value = "get bikes from a Station", notes = "bikes list")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Successful", response = Track.class, responseContainer="List of tracks"),
     })
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    //Tracks declarado in Impl tracks
-    public Response getTracks() {
-
-        //findAll declarado interficie
-        List<Track> tracks = this.tm.findAll();
-
-        GenericEntity<List<Track>> entity = new GenericEntity<List<Track>>(tracks) {};
-        return Response.status(201).entity(entity).build()  ;
-
+    
+    @Path("/sortbikesbykm/{idStation}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response bikesByStationOrderByKms(@PathParam("idStation") String idStation) {
+        try {
+            LinkedList<Bike> bikes = this.mb.bikesByStationOrderByKms(idStation);
+            GenericEntity<LinkedList<Bike>> entity = new GenericEntity<LinkedList<Bike>>(bikes){};
+            return Response.status(201).entity(entity).build();
+        } catch (StationNotFoundException e) {
+            e.printStackTrace();
+            return Response.status(404).build();
+        }
     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     //Dame un track
     @GET
     @ApiOperation(value = "get a Track", notes = "asdasd")
